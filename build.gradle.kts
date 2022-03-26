ce0951fefd9bbbff4b8ca8e67db74cd17594e075
YueFeng Huang <mcxinyu@foxmail.com>
2022-03-26 09:20:29 +0800

    1.0.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ ext {
 }
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-jdk.tools.jlink.resources.plugins {
+plugins {
     id("com.android.application") version "7.1.2" apply false
     id("com.android.library") version "7.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.6.10" apply false
