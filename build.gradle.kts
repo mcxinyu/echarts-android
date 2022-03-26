e2ab1e4b8d852f8675655aa08ea305163913332c
YueFeng Huang <mcxinyu@foxmail.com>
2022-03-26 09:05:17 +0800

    1.0.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import java.util.*
 
 ext {
-    extra["core_version"] = "1.0.5"
+    extra["core_version"] = "1.0.6"
 
     Properties().apply {
         load(project.rootProject.file("local.properties").inputStream())
@@ -12,7 +12,7 @@ ext {
 }
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
+jdk.tools.jlink.resources.plugins {
     id("com.android.application") version "7.1.2" apply false
     id("com.android.library") version "7.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.6.10" apply false
